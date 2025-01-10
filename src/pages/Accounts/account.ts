7097cd3a950da3b79c33f21182c5b6d47b28f66f
Pawacode Database <whakhydoh@gmail.com>
2025-01-10 02:29:23 +0100

    pp

--- src/pages/Accounts/account.ts
+++ src/pages/Accounts/account.ts
@@ -21,6 +21,41 @@ export interface GameAccount {
   import rabayii_5 from './images/rabayii_5.jpg'
   import rabayii_6 from './images/rabayii_6.jpg'
   
+
+  import omooo_1 from './images/omooo_1.jpg';
+  import omooo_2 from './images/omooo_2.jpg';
+  import omooo_3 from './images/omooo_3.jpg';
+  import omooo_4 from './images/omooo_4.jpg';
+  import omooo_5 from './images/omooo_5.jpg';
+  import omooo_6 from './images/omooo_6.jpg';
+
+
+
+  import mawee_1 from './images/maawe_1.jpg';
+import mawee_2 from './images/maawe_2.jpg';
+import mawee_3 from './images/maawe_3.jpg';
+import mawee_4 from './images/maawe_4.jpg';
+import mawee_5 from './images/maawe_5.jpg';
+import mawee_6 from './images/maawe_6.jpg';
+
+
+import mauve_1 from './images/mauve_1.jpg';
+import mauve_2 from './images/mauve_2.jpg';
+import mauve_3 from './images/mauve_3.jpg';
+import mauve_4 from './images/mauve_4.jpg';
+import mauve_5 from './images/mauve_5.jpg';
+import mauve_6 from './images/mauve_6.jpg';
+
+
+import nairamm_1 from './images/nairamm_1.jpg';
+import nairamm_2 from './images/nairamm_2.jpg';
+import nairamm_3 from './images/nairamm_3.jpg';
+import nairamm_4 from './images/nairamm_4.jpg';
+import nairamm_5 from './images/nairamm_5.jpg';
+import nairamm_6 from './images/nairamm_6.jpg';
+import nairamm_7 from './images/nairamm_7.jpg';
+
+
   export const accounts: GameAccount[] = [
     {
       id: 1,
@@ -53,7 +88,7 @@ export interface GameAccount {
         rabayii_1, rabayii_2, rabayii_3, rabayii_4,rabayii_5,rabayii_6
       ],
       videoLink: "https://t.me/Sultan_pubg_acc_Dealerr/1762",
-      status: true,
+      status: false,
       mythicsCount: 8,
       carsCount: 8,
       gunSkinsCount: 30,
@@ -64,21 +99,388 @@ export interface GameAccount {
     ,
 {
       id: 2,
-      name: "Pro Sniper Account",
-      price: 600,
-      accountFeatures: ["Special sniper rifle skins", "5 cars unlocked"],
-      moreFeat: [" Set", "Unlocked 5 cars"],
+      name: "#658",
+      price: 120,
+      accountFeatures: [
+
+        "LEVEL 6 GLACIER M4 ",
+        "LEVEL 4 SAVAGERY UZI",
+        "LEVEL 4 8 BIT M762 ",
+        "LEVEL 4 STEAM THOMPSON ",
+        "LEVEL 3 MARINE UMP",
+        "LEVEL 3 PANTERA DBS ",
+        "LEVEL 2 SKY HUNTRESS MG3",
+        "LEVEL 2 SPIRIT SENTRY UZI",
+        "LEVEL 1 SHINOBI KAMI M4 ",
+        "LEVEL 1 RIVER STYX GROZA",
+      ],
+      moreFeat: [
+       "⭐️⭐️ FIORE XSUIT ",
+       " ⭐️ GALADRIA XSUIT ",
+
+    " YELLOW MUMMY SET ✅"
+,
+    " NETHER VISAGE CYCLE ✅"
+
+      ],
      
       images: [
-        "https://example.com/images/account2-1.jpg",
-        "https://example.com/images/account2-2.jpg",
+        omooo_1, omooo_2, omooo_3, omooo_4, omooo_5, omooo_6
       ],
-      videoLink: "https://youtu.be/example2",
+      videoLink: "https://t.me/Sultan_pubg_acc_Dealerr/1787",
       status: true,
       mythicsCount: 5,
-      carsCount: 5,
-      gunSkinsCount: 15,
+      // carsCount: 5,
+      gunSkinsCount: 5,
       accountLevel: "Conquerer",
     },
+
+    
+  {
+    id: 3,
+    name: "657",
+    price: 120,
+    accountFeatures: [
+    "  LEVEL 5 GLACIER M4 ",
+    "  LEVEL 5 FOOL M4 ",
+    "  LEVEL 4 ENIGMATIC DP",
+    "  LEVEL 4 EMP UMP",
+     " LEVEL 4 MESSI ICON M762",
+     " LEVEL 3 MARINE UMP",
+     " LEVEL 3 SPIRIT SENTRY UZI",
+    ],
+    moreFeat: [
+
+      "ONE EXTRA MATERIALS ✅",
+   "   FREE 600UC AVAILABLE ✅",
+
+     " ⭐️ JESTER XSUIT ",
+     " ⭐️ IRIDESCENCE XSUIT ",
+
+    ],
+    images: [ mawee_1,
+      mawee_2,
+      mawee_3,
+      mawee_4,
+      mawee_5,
+      mawee_6,]
+      ,
+    videoLink: "https://t.me/Sultan_pubg_acc_Dealerr/1785",
+    status: true,
+    mythicsCount: 5,
+    carsCount: 5,
+    gunSkinsCount: 15,
+    accountLevel: "Conquerer",
+  },
+  {
+    id: 4,
+    name: "656",
+    price: 630,
+    accountFeatures: [
+      "OLD SEASON ACCOUNT ✅",
+
+"🌙 LEVEL 7 GLACIER M4",
+      "LEVEL 7 SKELENTAL M762",
+      "LEVEL 7 ABYSSAL AUG ",
+     " LEVEL 5 JACK LANTERN AKM",
+      "LEVEL 4 MAUVE AWM",
+      "LEVEL 4 CONCERTO M762 ",
+     " LEVEL 4 DROP BASS SCARL",
+     " LEVEL 4 DAZZLING QBZ",
+     " LEVEL 4 ROMANTIC UZI",
+     " LEVEL 4 KILLER TUNE M24",
+      "LEVEL 3 MID NIGHT VECTOR ",
+      "LEVEL 3 CROW VSS ",
+      
+
+     
+    ],
+    moreFeat: [
+
+      "SKELENTAL M762 ALL ATTACHMENTS UNLOCKED ✅",
+     " ABYSSAL HOWL AUG ALL ATTACHMENTS UNLOCKED ✅",
+ 
+       "11 EXTRA MATERIALS ✅",
+ 
+       "⭐️⭐️ PHAROAH XSUIT",
+      "      ⭐️ BLOOD RAVEN XSUIT",
+ 
+      " LAMBORGHINI URUS",
+      " ROBUST NIGHT CITY UAZ",
+      " TESLA ROADSTER ",
+       "TESLA  CYBER TRUCK",
+       "MC LAREN COUPE ",
+      " BUGATTI LA VOITURE",
+
+
+    ],
+    images: [
+      mauve_1,
+      mauve_2,
+      mauve_3,
+      mauve_4,
+      mauve_5,
+      mauve_6
+    ],
+    videoLink: "https://t.me/Sultan_pubg_acc_Dealerr/1783",
+    status: true,
+    mythicsCount: 20,
+    carsCount: 5,
+    gunSkinsCount: 20,
+    accountLevel: "Conquerer",
+  },
+  {
+    id: 5,
+    name: "655",
+    price: 260,
+    accountFeatures: [
+      
+      "LEVEL 7 FOOL M4",
+      "LEVEL 7 SILVER GURU M4",
+      "LEVEL 7 GLACIER AKM",
+      "LEVEL 4 GOLDEN VECTOR",
+      "LEVEL 4 CANDY THOMPSON",
+      "LEVEL 4 ENIGMATIC DP",
+      "LEVEL 4 8 BIT M762",
+      "LEVEL 4 DESERT AKM",
+      "LEVEL 4 ROMANTIC UZI",
+      "LEVEL 4 GODZILLA AWM",
+      "LEVEL 4 EMP UMP",
+      "LEVEL 4 SOLDIER SOUL BIZON",
+      "LEVEL 3 STINGER UMP",
+      "LEVEL 1 ADMIRAL AKM",
+      
+      
+    ],
+    moreFeat: [
+"CITY DEFENDER JACKET",
+"SNOWSTAR SWEETHEART SET",
+      "SILVER GURU SET",
+    ],
+    images: [ nairamm_1,
+      nairamm_2,
+      nairamm_3,
+      nairamm_4,
+      nairamm_5,
+      nairamm_6,
+      nairamm_7,],
+    videoLink: "https://t.me/Sultan_pubg_acc_Dealerr/1781",
+    status: true,
+    mythicsCount: 10,
+    carsCount: 1,
+    gunSkinsCount: 10,
+    accountLevel: "Conquerer",
+  },
+  // {
+  //   id: 6,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 7,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 8,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 9,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 10,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 11,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 12,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 13,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 14,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 15,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 16,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 17,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 18,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 19,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  // {
+  //   id: 20,
+  //   name: "Pro",
+  //   price: null,
+  //   accountFeatures: [],
+  //   moreFeat: [],
+  //   images: [],
+  //   videoLink: "",
+  //   status: true,
+  //   mythicsCount: 5,
+  //   carsCount: 5,
+  //   gunSkinsCount: 15,
+  //   accountLevel: "Conquerer",
+  // },
+  
   ];
   
